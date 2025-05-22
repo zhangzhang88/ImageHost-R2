910b9868ae6f64a01860fab3159a3ed6e70a4c68
zhangzhang88 <68057668+zhangzhang88@users.noreply.github.com>
2025-05-22 21:02:45 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 async function fetchConfig() {
-  const res = await fetch("https://api.nbvil.com/config");
+  const res = await fetch("https://image.ztr8.uk");
   if (!res.ok) throw new Error("获取配置失败");
   return await res.json();
 }
